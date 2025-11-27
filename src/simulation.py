076ffc70140b5cd34675153d56abe94bbafcc8ba
ashtynwhipple <156721379+ashtynwhipple@users.noreply.github.com>
2025-11-26 23:54:46 -0700

    first simple simulation

---
+++ src/simulation.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def simulate_particle(mu, slab_thickness):
+    """
+    Parameters:
+    mu - probability distribution
+    slab_thickness - thickness of layer that protons are going through
+
+    Returns:
+    boolean: true if disance > slab thickness
+    i.e. true if the proton made it through the layer
+    """
+    distance = -np.log(np.random.rand()) / mu # this is sampling from and exponential probability distribution
+
+    return distance > slab_thickness # returns true if the proton made it through
+
+def run_simulation(N, mu, slab_thickness):
+    """
+    Parameters:
+    N - number of protons wishing to simulate
+    mu - probability distribution
+    slab_thickness - thickness of layer that protons are going through
+
+    Returns:
+    count_transmitted: number of protons that went through the material
+    """
+    count_transmitted = 0
+    for _ in range(N):
+        if simulate_particle(mu, slab_thickness):
+            count_transmitted += 1
+    return count_transmitted
+
+print(run_simulation(N=10000, mu=0.5, slab_thickness=1.0) / 10000)
+print(np.e ** (-0.5))
